22ee440d693f1fb9de95b0f7f05e8dc617938648
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-02 03:28:38 +0100

    class filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+""" user logging details """
+import logging
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """ a function that returns logging info """
+    fields =
+    redaction =
+    message =
+    seperator = 
+    return re.sub(
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname0s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+      
+        
+
+
+
+
