beaac669c862e0102117ba7fe88ae04e817dc8b1
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-02 04:22:28 +0100

    hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+""" a script that encripts """
+
+import bcrypt
+
+
+def is_valid(hashed_password, password):
+    """ hashing password mechanism """
+    return hashed_password
