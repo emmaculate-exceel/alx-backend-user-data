a3057dcf61f32b8d230481f2b416c416fd8fd2f2
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-03 08:50:33 +0100

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ import re
 def filter_datum(fields, redaction, message, separator):
     """ a function that returns logging info """
     pattern = '|'.join([f"{field}=[^{separator}]+"
-                        for field in fields])  
+                        for field in fields])
     return re.sub(pattern, lambda match: match.group(0).split(
         '=')[0] + '=' + redaction, message)
 
@@ -19,13 +19,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname0s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-
     def __init__(self):
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
         NotImplementedError
-      
-        
+
+
 def main:
     return get_db
