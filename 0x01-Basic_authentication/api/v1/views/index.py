34cbe1a1006dfa575c312288150796b40c3b720f
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-07 17:10:52 +0100

    update

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,8 +5,8 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/api/v1/unauthorized', method=['GET'])
-def unauthorized():
+@app_views.route('/api/v1/unauthorized', method=['GET'], strict_slashes=False)
+def unauthorized() -> str:
     """ unauthorized access
     Return :
       - abortive response
