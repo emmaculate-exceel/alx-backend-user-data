b9cad0d33082bc5c3c185739cfe26102714663f9
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-13 18:16:15 +0100

    userModel

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" creating an SQLAlchemy model """
+import sqlalchemy
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=False)
+    reset_token = Column(String, nullable=False)
