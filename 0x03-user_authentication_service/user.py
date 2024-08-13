3d0bc358b37ef5ca3e115ed9e4d54faed7544e71
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-13 18:44:59 +0100

    userModel

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,3 +18,6 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=False)
     reset_token = Column(String, nullable=False)
+
+    def __repr__(self):
+        return f"<User(email='{self.email}', hashed_password='{self.hashed_password}', session_id='{self.session_id}', reset_token='{self.reset_token}')>"
