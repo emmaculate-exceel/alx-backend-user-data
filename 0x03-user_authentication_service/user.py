f7144b68a6d9ca45df9ab6ec63957f785575895e
emmaculate-exceel <emmaculate2exceel@live.com>
2024-08-14 06:10:19 +0100

    update users

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,14 +14,12 @@ class User(Base):
     """
     creating a user model
     """
-    __tablename__ = 'users'
-    
+    __tablename__ = 'users'    
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
     """ def __repr__(self):
          #string representation of object
 
